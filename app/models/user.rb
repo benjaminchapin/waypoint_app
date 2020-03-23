371b9328bba743f2f6f4530a877f72de1f7c51f6
ben <benjamin.n.chapin@gmail.com>
2020-03-23 14:55:02 -0500

    edits hike advanced partial to include waypoint image url

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :waypoints
 
   has_secure_password
+  validates :password, length: { in: 4..15 }
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
